e0b050444dd9865db432a4b17b57cbf5fc28ff67
Jorge Andrés Mena <jormencar@yahoo.com>
2021-07-19 21:07:03 -0400

    adding filters

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,40 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from '../actions';
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((i) => i.id === id + color);
 
-export default cart_reducer
+    if (tempItem) {
+      const tempCart = state.cart.map((item) => {
+        if (item.id === id + color) {
+          let newAmount = item.amount + amount;
+          if (newAmount > item.max) {
+            newAmount = item.max;
+          }
+          return { ...item, amount: newAmount };
+        } else {
+          return item;
+        }
+      });
+
+      return { ...state, cart: tempCart };
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
